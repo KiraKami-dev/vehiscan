8b6687816c166512ae831f85f4da61aa4f454753
KiraKami <akshatkhair41@gmail.com>
2023-10-31 03:27:05 +0530

    login check

--- lib/src/widgets/drawer.dart
+++ lib/src/widgets/drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:vehiscan/src/screens/admin_record.dart/admin_login.dart';
 import 'package:vehiscan/src/screens/home_screen.dart';
 import 'package:vehiscan/src/screens/user_resgistration/user_reg.dart';
@@ -14,10 +15,21 @@ class NavDrawer extends StatefulWidget {
 }
 
 class _NavDrawerState extends State<NavDrawer> {
+  bool _isLogin = false;
   @override
   Widget build(BuildContext context) {
     final buildiingName = LocalStorageService.getSelectedBuilding();
-    return Drawer(
+    if (buildiingName.isNotEmpty) {
+      setState(() {
+        _isLogin = true;
+      });
+    } else {
+      setState(() {
+        _isLogin = false;
+      });
+    }
+
+    return _isLogin? Drawer(
       backgroundColor: Colors.white,
       elevation: 2,
       child: ListView(
@@ -30,7 +42,15 @@ class _NavDrawerState extends State<NavDrawer> {
             child: Image.network(
                 "https://img.freepik.com/free-vector/front-car-concept-illustration_114360-7978.jpg?w=1380&t=st=1696513992~exp=1696514592~hmac=14f7a429d85bf7aed10af0579c9f9c1690f829aeb184f6615d31cd37868fe825"),
           ),
-          Text(buildiingName,textAlign: TextAlign.center,),
+          Text(
+            _isLogin ? buildiingName : "Building",
+            style: TextStyle(fontSize: 20),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Divider(),
           ListTile(
             trailing: Icon(IconlyBold.scan),
             title: const Text('Scan'),
@@ -42,6 +62,77 @@ class _NavDrawerState extends State<NavDrawer> {
               );
             },
           ),
+          ListTile(
+            trailing: Icon(
+              IconlyBold.user2,
+              color: Colors.black,
+            ),
+            title: Text(
+              'Records',
+              style: TextStyle(color: Colors.black),
+            ),
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => AdminLogin(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            trailing: Icon(
+              IconlyBold.logout,
+              color: Colors.redAccent,
+            ),
+            title: Text(
+              'Log Out',
+              style: TextStyle(color: Colors.redAccent),
+            ),
+            onTap: () {
+              LocalStorageService.clear();
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => HomeScreen(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    ) :Drawer(
+      backgroundColor: Colors.white,
+      elevation: 2,
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            // decoration: BoxDecoration(
+            //   color: Colors.blue,
+            // ),
+            child: Image.network(
+                "https://img.freepik.com/free-vector/front-car-concept-illustration_114360-7978.jpg?w=1380&t=st=1696513992~exp=1696514592~hmac=14f7a429d85bf7aed10af0579c9f9c1690f829aeb184f6615d31cd37868fe825"),
+          ),
+          Text(
+            _isLogin ? buildiingName : "VehiScan",
+            style: TextStyle(fontSize: 20),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Divider(),
+          ListTile(
+            trailing: Icon(IconlyBold.scan),
+            title: const Text('Scan'),
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => HomeScreen(),
+                ),
+              );
+            },
+          ),
+          
           ListTile(
             trailing: Icon(
               IconlyBold.user2,
@@ -75,9 +166,9 @@ class _NavDrawerState extends State<NavDrawer> {
                 ),
               );
             },
-          ),
+          ),          
         ],
       ),
-    );
+    ) ;
   }
 }
